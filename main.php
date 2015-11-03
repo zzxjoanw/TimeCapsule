4449cfcb96066b85e5ec0700614bdcaadf973c13
zzxjoanw <zzxjoanw@gmail.com>
2015-11-03 20:42:03 +0000

    initial commit

--- main.php
+++ main.php
@@ -11,7 +11,39 @@
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <div id="nav"></div>
+        <div id="nav">
+            <div id="logo">logo</div>
+            <?
+                if(!isset($_SESSION))
+                {
+                    ?>
+                        <div id="login">login</div>
+                        <div id="register">register</div>
+                    <?
+                }
+                elseif($role == "student")
+                {
+                    ?>
+
+                    <?
+                }
+                elseif($role=="parent")
+                {
+                    ?>
+                    <?
+                }
+                elseif($role=="school")
+                {
+                    ?>
+                    <?
+                }
+                elseif($role=="admin")
+                {
+                    ?>
+                    <?
+                }
+            ?>
+        </div>
         <div id="main"></div>
     </body>
 </html>
