3b3c23c819b59843a410bad1292d5cf94d1bc8e9
zzxjoanw <zzxjoanw@gmail.com>
2015-11-24 22:04:34 +0000

    initial commit for reg-test.php

---
+++ reg-test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Laura 4
+ * Date: 11/24/2015
+ * Time: 9:58 PM
+ */
+
+include("includes/db-functions.php");
+
+$mysqli = doConnect();
+$sql = "INSERT INTO studentTable (firstname,lastname,email,password) VALUES('Laura','Mayer-Sommer','zzxjoanw@gmail.com','test')";
+doQuery($sql,$mysqli);
