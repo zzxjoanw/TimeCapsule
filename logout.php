ff7e79ea6fceec3676bbc102051d629f38ff5693
zzxjoanw <zzxjoanw@gmail.com>
2015-12-13 23:03:31 +0000

    first commit for logout page

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Laura 4
+ * Date: 12/13/2015
+ * Time: 11:00 PM
+ */
+
+include("includes/db-functions.php");
+
+logout();
+
+header("location:main.php");
+
+?>
