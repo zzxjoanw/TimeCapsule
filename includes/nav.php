b2c7e37e995b9fd3f8809dbc38c728866f53d59d
zzxjoanw <zzxjoanw@gmail.com>
2015-12-13 23:04:52 +0000

    nav.php checks whether user is logged in or not, responds appropriately

--- includes/nav.php
+++ includes/nav.php
@@ -7,9 +7,9 @@
         <div id="login">
             <div id="login-open">
                 <form action="main.php" method="post">
-                    <input type="text" class="form-control" name="username" placeholder="Username">
+                    <input type="text" class="form-control" name="email" placeholder="Email">
                     <input type="password" class="form-control" name="password" placeholder="Password">
-                    <button type="submit" class="btn btn-submit" name=""bttnLogin">Submit</button>
+                    <button type="submit" class="btn btn-submit" name="bttnLogin">Submit</button>
                 </form>
             </div>
             <span>login</span>
@@ -19,26 +19,9 @@
         </div>
         <?
     }
-    elseif($role == "student")
+    else
     {
-        ?>
-
-        <?
-    }
-    elseif($role=="parent")
-    {
-        ?>
-        <?
-    }
-    elseif($role=="school")
-    {
-        ?>
-        <?
-    }
-    elseif($role=="admin")
-    {
-        ?>
-        <?
+        echo "logged in. <a href='profile.php'>go to profile</a> or <a href='logout.php'>log out</a>";
     }
     ?>
 </div>
