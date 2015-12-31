e09e12d655f60c383a3cb1d0c8bd2b6a67488e89
zzxjoanw <zzxjoanw@gmail.com>
2015-12-31 12:33:44 +1100

    user's interests and all other interests now gotten from getMyInterests() and getOtherInterests(). Country gotten from session variable instead of object.

---
+++ profile.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Laura 4
+ * Date: 11/29/2015
+ * Time: 8:36 PM
+ */
+
+session_start();
+if(!isset($_SESSION['firstname']))
+{
+    header('Location: main.php') ;
+}
+
+include("includes/db-functions.php");
+
+$connection = openDBConnection();
+?>
+
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
+    <script language="JavaScript">
+        $(document).ready(function(){
+            $("#login-open").hide();
+            $("#register-open").hide();
+
+            $("#login span").click(function() {
+                $("#login-open").toggle();
+                var isLoginShown = $("#login-open").is(":visible");
+            });
+        });
+    </script>
+</head>
+<body>
+    <?
+        include("includes/nav.php");
+        $myInterestList = getMyInterests($connection);
+        $otherInterestsList = getOtherInterests($connection);
+    ?>
+    <div id="main">
+        <hr>
+        Interests <br/>
+        <table>
+            <tr>
+                <td>
+                    <form action="profile.php" method="post">
+                    <?
+                        for($i=0;$i<count($myInterestList);$i++)
+                        {
+                            echo "<input type='checkbox' name='addList' value='<? $myInterestList[$i] ?>'>" . $myInterestList[$i];
+                        }
+                    ?>
+                    </form>
+                </td>
+                <td>
+                    <?
+                        for($i=0;$i<count($otherInterestsList);$i++)
+                        {
+                            echo $otherInterestsList[$i];
+                            echo "<a href='#' style='float:right'>add</a><br/>";
+                        }
+                    ?>
+                </td>
+            </tr>
+        </table>
+        <hr>
+        GCSEs <br/>
+        <?
+            $country = $_SESSION['country'];
+            $gcseList = getGCSEList($connection,$country);
+        ?>
+    </div>
+</body>
+</html>
