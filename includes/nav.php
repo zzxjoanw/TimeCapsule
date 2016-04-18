9c6d68e87c2070004d28deb7df21549c7371f883
zzxjoanw <zzxjoanw@gmaill.com>
2016-04-18 09:04:33 +0100

    adding spacing to greeting div\n greeting only displays when user logged in

--- includes/nav.php
+++ includes/nav.php
@@ -46,6 +46,13 @@
 
 <div id="nav">
     <div id="logo">Time Capsule</div>
+    <?
+        if(isset($_SESSION['studentID']))
+        {
+            ?> <div id="greeting">Hello, <? echo $_SESSION['firstname']; ?></div> <?
+        }
+    ?>
+
     <div id="navButtons">
             <?
             if(!isset($_SESSION))
@@ -63,19 +70,37 @@
                 </a>
                 <!--<a href="#" id="registerBttn">-->
                 <a href="register.php">
-                    <svg height="100%" viewBox="0 0 50 50" id="loginSVG">
-                        <circle cx="25" cy="25" r="25" stroke="none" fill="blue" class="background" />
-                        <line x1="10" x2="40" y1="25" y2="25" style="stroke:white; stroke-width:4" />
-                        <line x1="25" x2="25" y1="10" y2="40" style="stroke:white; stroke-width:4" />
-                    </svg>
+                    <svg height="100%" viewBox="0 0 50 50" id="regSVG">
+                    <circle cx="25" cy="25" r="25" stroke="none" fill="blue" class="background" />
+                    <line x1="10" x2="40" y1="25" y2="25" style="stroke:white; stroke-width:4" />
+                    <line x1="25" x2="25" y1="10" y2="40" style="stroke:white; stroke-width:4" />
+                </svg>
                 </a>
             <?
         }
         else
         {
             ?>
-            <a href="profile.php"><img src="images/profile.png" alt="Profile"></a>
-            <a href="logout.php"><img src="images/logout.png" alt="Logout"></a>
+            <a href="profile.php">
+                <svg height="100%" viewBox="0 0 50 50" id="profileSVG">
+                    <circle cx="25" cy="25" r="25" stroke="none" fill="blue"/>
+                    <line x1="25" x2="25" y1="5" y2="45" stroke="white" stroke-width="4"/>
+                    <line x1="10" x2="40" y1="10" y2="40" stroke="white" stroke-width="4"/>
+                    <line x1="5" x2="45" y1="25" y2="25"  stroke="white" stroke-width="4"/>
+                    <line x1="40" x2="10" y1="10" y2="40"  stroke="white" stroke-width="4"/>
+                    <circle cx="25" cy="25" r="15" stroke="white" stroke-width="4" fill="blue"/>
+                </svg>
+            </a>
+            <a href="logout.php">
+                <svg height="100%" viewBox="0 0 50 50" id="profile">
+                    <circle cx="25" cy="25" r="25" stroke="none" fill="blue" class="background" />
+                    <polyline points="15,10, 35,10, 35,40, 15,40" style="stroke:white; stroke-width:4; fill:none" class="door"/>
+                    <g id="arrow">
+                        <line x1="25" y1="25" x2="10" y2="25" style="stroke:white; stroke-width:4" class="arrowSpine"/>
+                        <polyline points="20,20,25,25,20,30" style="stroke:white; stroke-width:4; fill:none" class="arrowPoint" />
+                    </g>
+                </svg>
+            </a>
             <?
         }
         ?>
